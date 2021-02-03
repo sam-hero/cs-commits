1295de0dc14ec6fa4302bc7a1d8602562937690a
Samuel Herrera <sherrera@solidemx.com>
2021-02-03 12:43:01 -0700

    Layout of Home
    Preparing the layout for the view Home, this is going to show Information of Repo, Count of Commits, and a Graph of commits
    
    Changes:
    Home.js Implementing Row and Col from Bootstrap to make a simple layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,40 @@
+import { Container, Row, Col, Card } from "react-bootstrap";
+
 function Home(){
     return(
         <div>
             <h1>Home view</h1>
+            <Container>
+                <Row>
+                    <Col> 
+                        <Card>
+                            <h5>
+                                Information of Repo 
+                                Name:
+                            </h5>
+                            <Card.Body>
+                                <p>Description:</p>
+                                <p>Created at:</p>
+                                <p>Created By:</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <br/>
+                <Row>
+                    <Col xs="6">
+                        <Card>
+                            <h5>Total Commits</h5>
+                        </Card>
+                    </Col>
+
+                    <Col xs="6">
+                        <Card>
+                            <h5> Graph of Commits</h5>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
