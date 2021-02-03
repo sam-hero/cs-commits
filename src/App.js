f58ee7f12f1e7068424864aad294f473059af460
Samuel Herrera <sherrera@solidemx.com>
2021-02-03 12:16:44 -0700

    Create Home Component
    Add an Empty component name Home to show the main information about the repo cs-commits
    
    Changes:
    App.js import Home
    Home.js Create Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <h1>sam-hero</h1>
+      <Home/>
     </div>
   );
 }
