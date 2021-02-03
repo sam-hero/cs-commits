426bfa859501d95dbce77a71baf8d295b8aead46
Samuel Herrera <sherrera@solidemx.com>
2021-02-03 12:54:08 -0700

    Layout of Profile
        Preparing the layout for the Profile view, this is going to show Information of User
    
        Changes:
        Profile.js Implementing Row,Col, Card from Bootstrap to make a simple layout

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home';
 import Profile from './components/Profile';
 import Commits from './components/Commits';
-import { Container, Nav } from 'react-bootstrap';
+import { Container, Nav, Row } from 'react-bootstrap';
 import { Link,Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>sam-hero</h1>
-      <Nav className="mr-auto">
-              <Nav.Link as={Link} to={'/'}>Home</Nav.Link>
-              <Nav.Link as={Link} to={'profile'}>Profile</Nav.Link>
-              <Nav.Link as={Link} to={'commits'}>Commits</Nav.Link>
-          </Nav>
+      <Row class="justify-content">
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to={'/'}>Home</Nav.Link>
+          <Nav.Link as={Link} to={'profile'}>Profile</Nav.Link>
+          <Nav.Link as={Link} to={'commits'}>Commits</Nav.Link>
+        </Nav>
+      </Row>
       <Container>
         <Route exact={true} path="/" component={Home}/>
         <Route exact={true} path="/profile" component={Profile}/>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,39 @@
+import { Card, Col, Container, Row, Image } from "react-bootstrap";
+
 function Profile(){
     return (
         <div>
             <h1>Profile view</h1>
+            <Container>
+                <Row>
+                    <Col>
+                        <Card>
+                            <h5>User Information</h5>   
+                            <Image src="holder.js/171x180" roundedCircle />
+                                
+                            <Card.Body>
+                                <p>Username:</p>
+                                <p>Bio:</p>
+                                <p>Created at:</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <br/>
+                <Row>
+                    <Col>
+                        <Card>
+                            <h5> All About Him</h5>
+                            <Card.Body>
+                                <p>Company:</p>
+                                <p>Location:</p>
+                                <p>Followers:</p>
+                                <p>Twitter:</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
