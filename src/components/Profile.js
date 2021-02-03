1ceb2f57628c05a2ca1ee44d70329408b863e497
Samuel Herrera <sherrera@solidemx.com>
2021-02-03 13:47:22 -0700

    Fetching information of user sam-hero
        In the Profile component we are fetching the information from the github API of the user sam-hero
    
        Changes:
        Profile
    
        Implement State using useState
        Implement methods of lifecycle using the useEffect and useLayoutEffect to the component to call the latest information of user profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,20 +1,51 @@
 import { Card, Col, Container, Row, Image } from "react-bootstrap";
+import {useState, useEffect, useLayoutEffect} from 'react';
+import Axios from 'axios';
+import '../App.css';
+
 
 function Profile(){
+
+    const [user,setUser] = useState(null);
+    const [firstTime,setFirstTime] = useState(true);
+
+    useEffect( () => {
+        if(firstTime){
+            console.log('Buscaré la info');
+            setFirstTime(false);
+        }
+        return() => {
+            console.log('Salí de pantalla');
+        }
+    },[firstTime]);
+
+    useLayoutEffect( () => {
+        getData();
+    });
+
+    function getData(){
+        if(firstTime){
+            Axios.get('https://api.github.com/users/sam-hero').then( (response) => {
+                setUser(response.data);
+            });
+        }
+    }
     return (
         <div>
             <h1>Profile view</h1>
-            <Container>
+            { user ? 
+                <Container>
                 <Row>
                     <Col>
                         <Card>
                             <h5>User Information</h5>   
-                            <Image src="holder.js/171x180" roundedCircle />
                                 
                             <Card.Body>
-                                <p>Username:</p>
-                                <p>Bio:</p>
-                                <p>Created at:</p>
+                                <Image className="img-thumbnail user-logo" src={user.avatar_url} roundedCircle />
+                                <p>name: {user.name}</p>
+                                <p>Username: {user.login}</p>
+                                <p>Bio: { user.bio }</p>
+                                <p>Created at: {user.created_at}</p>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -25,15 +56,17 @@ function Profile(){
                         <Card>
                             <h5> All About Him</h5>
                             <Card.Body>
-                                <p>Company:</p>
-                                <p>Location:</p>
-                                <p>Followers:</p>
-                                <p>Twitter:</p>
+                                <p>Company: {user.company}</p>
+                                <p>Location: {user.location}</p>
+                                <p>Followers: {user.followers}</p>
+                                <p>Twitter: {user.twitter_username}</p>
                             </Card.Body>
                         </Card>
                     </Col>
                 </Row>
             </Container>
+            : 
+            <span> Loading...</span> }
         </div>
     );
 }
