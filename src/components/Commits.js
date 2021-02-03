82fa4aa3ce6005850d1f6f420175e643bc69fce2
Samuel Herrera <sherrera@solidemx.com>
2021-02-03 13:02:50 -0700

    Layout of Commits
            Preparing the layout for the Commits view, this is going to show a list of information and when a commits is selected, it will show full information
    
            Changes:
            Commit.js Implementing Row,Col,Card, ListGroup from Bootstrap to make a simple layout

--- src/components/Commits.js
+++ src/components/Commits.js
@@ -1,9 +1,29 @@
-function Commits(){
-    return (
-        <div>
-            <h1>Commits View</h1>
-        </div>
+import {Row, Col , ListGroup , Card} from 'react-bootstrap'
 
+function Commits(){
+    return(
+        <div>
+            <h1>Profile View</h1>
+            <Row>
+                <Col>
+                        <h6>Commit List </h6>
+
+                        <ListGroup>
+                            <ListGroup.Item>
+                                Commit
+                            </ListGroup.Item>
+                        </ListGroup>                    
+                </Col>
+                <Col>
+                    <h6>Commit Information</h6>
+                    <Card>
+                        <Card.Body>
+                            Info
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </div>
     );
 
 }
